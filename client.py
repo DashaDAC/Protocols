e2239e3793b68713a9f4253dd61ff12259173e5e
darya <darya.sen1@mail.ru>
2022-07-07 23:38:11 +0300

    add file

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+IP = "127.0.0.1"  # ip (localhost)
+PORT = 65432  # Port to listen
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((IP, PORT))
+mail = input("Enter: ")
+server.send(bytes(mail, "utf-8"))
+buffer = server.recv(1024)
+buffer = buffer.decode("utf-8")
+print(f"Server: {buffer}")
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+IP = "127.0.0.1"  # ip (localhost)
+PORT = 65432  # Port to listen
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen(5)
+while True:
+    client, address = server.accept()
+    print(f"Connected by {address[0]}:{address[1]}")
+
+    mail = client.recv(1024)
+    mail = mail.decode("utf-8")
+    print(mail)
+    client.send(bytes(mail, "utf-8"))
+
+    client.close()
