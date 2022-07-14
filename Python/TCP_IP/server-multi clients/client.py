35ece5f9b7af592d09062e68c24eb562f9eb315a
darya <darya.sen1@mail.ru>
2022-07-14 23:42:58 +0300

    add file

--- Python/TCP_IP/server-multi clients/client.py
+++ Python/TCP_IP/server-multi clients/client.py
@@ -1,12 +1,43 @@
 import socket
+import threading
 
-IP = "127.0.0.1"  # ip (localhost)
+def hangle_client(client, address):
+    print(f"[NEW CONNECTIONS] {address} connected")
+
+    connected = True
+    while connected:
+        mail = client.recv(1024)
+        mail = mail.decode("utf-8")
+        if mail == DISCONNECT_MAIL:
+            connected = False
+        print(mail)
+        client.send(bytes(mail, "utf-8"))
+
+    client.close()
+
+
+
+IP = socket.gethostbyname(socket.gethostname())
 PORT = 1234  # Port to listen
+DISCONNECT_MAIL = "!DISCONNECT"
+
+client = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((IP, PORT))
-mail = input("Enter:")
-server.send(bytes(mail, "utf-8"))
-buffer = server.recv(1024)
-buffer = buffer.decode("utf-8")
-print(f"Server: {buffer}")
+server.bind((IP, PORT))
+server.listen(5)
+while True:
+    client, address = server.accept()
+    thread = threading.Thread(target=hangle_client, args=(client, address))
+    thread.start()
+    print(f"Connected by {address[0]}:{address[1]}")
+    print(f"active connections {threading.activeCount()-1}")
+
+
+
+    # mail = client.recv(1024)
+    # mail = mail.decode("utf-8")
+    # print(mail)
+    # client.send(bytes(mail, "utf-8"))
+    #
+    # client.close()
--- Python/TCP_IP/server-multi clients/server.py
+++ Python/TCP_IP/server-multi clients/server.py
@@ -1,7 +1,25 @@
 import socket
+import threading
 
-IP = "127.0.0.1"  # ip (localhost)
+def hangle_client(client, address):
+    print(f"[NEW CONNECTIONS] {address} connected")
+
+    connected = True
+    while connected:
+        mail = client.recv(1024)
+        mail = mail.decode("utf-8")
+        if mail == DISCONNECT_MAIL:
+            connected = False
+        print(mail)
+        client.send(bytes(mail, "utf-8"))
+
+    client.close()
+
+
+
+IP = socket.gethostbyname(socket.gethostname())
 PORT = 1234  # Port to listen
+DISCONNECT_MAIL = "!DISCONNECT"
 
 client = []
 
@@ -10,11 +28,16 @@ server.bind((IP, PORT))
 server.listen(5)
 while True:
     client, address = server.accept()
+    thread = threading.Thread(target=hangle_client, args=(client, address))
+    thread.start()
     print(f"Connected by {address[0]}:{address[1]}")
+    print(f"active connections {threading.activeCount()-1}")
 
-    mail = client.recv(1024)
-    mail = mail.decode("utf-8")
-    print(mail)
-    client.send(bytes(mail, "utf-8"))
 
-    client.close()
+
+    # mail = client.recv(1024)
+    # mail = mail.decode("utf-8")
+    # print(mail)
+    # client.send(bytes(mail, "utf-8"))
+    #
+    # client.close()
