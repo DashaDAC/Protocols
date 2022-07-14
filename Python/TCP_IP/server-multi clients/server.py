ce4f17ecc2cf8d13fc12f4cb27dbd5c0861f3c87
darya <darya.sen1@mail.ru>
2022-07-14 23:21:26 +0300

    add file

---
+++ Python/TCP_IP/server-multi clients/server.py
@@ -0,0 +1,20 @@
+import socket
+
+IP = "127.0.0.1"  # ip (localhost)
+PORT = 1234  # Port to listen
+
+client = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen(5)
+while True:
+    client, address = server.accept()
+    print(f"Connected by {address[0]}:{address[1]}")
+
+    mail = client.recv(1024)
+    mail = mail.decode("utf-8")
+    print(mail)
+    client.send(bytes(mail, "utf-8"))
+
+    client.close()
